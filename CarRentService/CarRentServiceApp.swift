8483b85dce37e0365e08b43299613877954fb9f1
Aram <aramsaribekyan1@gmail.com>
2024-05-23 11:10:33 +0400

    Initial Commit

---
+++ CarRentService/CarRentServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarRentServiceApp.swift
+//  CarRentService
+//
+//  Created by Aram on 23.05.24.
+//
+
+import SwiftUI
+
+@main
+struct CarRentServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
